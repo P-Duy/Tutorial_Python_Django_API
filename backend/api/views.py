5bbe48361cdc68c8effe8f9330324836c5f2c5d0
PhamVanDuy <vanduy342001@gmail.com>
2023-04-18 16:41:02 +0700

    Echo GET Data

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,24 @@
+import json
 from django.http import JsonResponse
 
 def api_home(request, *args, **kwargs):
+    # requset -> HttpRequest-> Django
+    # print(dir(request))
+    # request.body
+    print(request.GET) #url query params
+    print(request.POST) #
+    body = request.body #by string of JSON data
+    data = {}
+    try:
+        data = json.loads(body) # string of JSON data -> python dict
+        #Để chuyển đổi chuỗi bytes này thành một đối tượng JSON trong Python, ta có thể sử dụng module json của Python và phương thức loads() để phân tích cú pháp JSON
+    except:
+        pass
+    print(data)
+    #data['headers'] = request.headers #request.META
+    data['params']= dict(request.GET)
+    data['headers']= dict(request.headers)
+    data['content_type'] = request.content_type
     
-    context={"message":"day la demo api"}
-    return JsonResponse(context)
+    #context={"message":"day la demo api"}
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -4,14 +4,14 @@ import requests
 # endpoint = "http://httpbin.org/anything"
 endpoint = "http://localhost:8000/api/" #http://127.0.0.1:8000/
 
-get_reponse = requests.get(endpoint, json ={"query":"Hello All"}) # HTTP Request
+get_reponse = requests.get(endpoint, params= {"product_id":123}) # HTTP Request
 print(get_reponse.text) # print raw text response
-print(get_reponse.status_code) # print status code
+#print(get_reponse.status_code) # print status code
 
 
 # HTTP Request -> HTML
 # REST API HTTP Request -> JSON
 # JavaScript Object Nototion ~Python Dict
 
-print(get_reponse.json()['message']) # print JSON response
+#print(get_reponse.json()) # print JSON response
 # print(get_reponse.status_code) # print status code

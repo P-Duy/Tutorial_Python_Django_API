7c0745f2a907c893ae777fde1126a9406cf82a26
PhamVanDuy <vanduy342001@gmail.com>
2023-05-13 14:21:30 +0700

    Rest Frameword view & response

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,26 +1,23 @@
 import json
 from django.forms.models import model_to_dict
-from django.http import JsonResponse, HttpResponse
+# from django.http import JsonResponse, HttpResponse
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from products.models import Product
 
+@api_view(["GET", "POST"])
 def api_home(request, *args, **kwargs):
+    """
+    DRF API View
+    """
+    if request.method != "POST":
+        return Response({"details": "GET not allowed"}, status=405)
     model_data = Product.objects.all().order_by("?").first()
     
     data={}
     if model_data:
         
         data= model_to_dict(model_data, fields='id,title,price')
-    return JsonResponse(data)
-    #     # print (data)
-    #     #data= dict(data)
-    #     # data['id'] = model_data.id
-    #     # data['title'] = model_data.title
-    #     # data['content'] = model_data.content
-    #     # data['price'] = model_data.price
-        
-    #     # model instance (model_data)
-    #     # turn a python dict
-    #     # return JSON to my client
-    #     json_data_str = json.dumps(data)
-    # return HttpResponse(json_data_str, headers={'Content-Type': 'application/json'})
+    return Response(data)
